9138b4c77cdbffcc9b83b2d24c05f63ceb046068
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-10-30 08:07:15 -0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,18 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
